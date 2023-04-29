2c1097d1ce535cac1db0915dba8496a4279c5721
Krlos Villanueva <krlosvilla101994@gmail.com>
2023-04-29 19:09:47 -0400

    0.0.3

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const helmet = require("helmet");
 
 const swaggerDocs = require('./captcha-docs.js');
 
@@ -16,8 +20,11 @@ dotenv.config()
 // Conexión a la base de datos
 connectDB();
 
-// Middleware para manejar las solicitudes JSON
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cors());
+app.use(helmet());
 
 //Swagger
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
@@ -26,10 +33,20 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/captcha', captchaRoutes);
 app.use('/healthcheck', healthcheckRoutes);
 
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).send("Something broke!");
+});
 
 // Puerto de escucha del servidor
 const PORT = process.env.PORT || 3000;
 
+process.on("uncaughtException", (err) => {
+	// Exit the app with error status.
+	try {
+		console.log("*** uncaughtException:", err);
+	} catch (e) {}
+});
 // Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
